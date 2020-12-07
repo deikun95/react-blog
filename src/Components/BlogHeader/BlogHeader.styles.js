cdd20bdac137e4cce7780caad4f3035a7cca80ba
Deikun95 <deikun95@gmail.com>
2020-12-07 22:33:54 +0300

    styled header

--- src/Components/BlogHeader/BlogHeader.styles.js
+++ src/Components/BlogHeader/BlogHeader.styles.js
@@ -26,20 +26,42 @@ export const HeaderBtn = styled.div`
     margin-right: 40px;
     height: 40px;
     fill: white;
-    transition: all ease 0.2s;
+    transition: all ease 0.3s;
     &:hover {
         fill: #6be8a3;
     }
     cursor: pointer;
 `;
 
+export const HeaderFind = styled.div`
+    margin-left: -70px;
+    width: 250px;
+    height: 45px;
+    display: flex;
+    background: #fff;
+    align-items: center;
+    border-radius: 10px;
+    justify-content: space-around;
+    transition: all ease 0.3s;
+`;
 export const FindIcon = styled.div`
     height: 30px;
     width: 30px;
-    fill: white;
+    fill: #505050;
+    margin-left: 10px;
+`;
+export const FindInput = styled.input`
+    outline: none;
+    border: none;
+    font-size: 18px;
+    padding: 0px 5px;
+    color: #505050;
+    width: 70%;
+    &:focus {
+        ${FindIcon} {
+            fill: red;
+        }
+    }
 `;
 
-export const HeaderFind = styled.div`
-    width: 150px;
-    height: 40px;
-`
+console.log(FindIcon);
